8c6a2699796988c0dfb5d0725e32d1ff48a4a29a
myxo <klimov.ns@mail.ru>
2022-07-02 10:44:58 +0300

    Add tmux script for testing

---
+++ run_in_tmux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo Building go code...
+go build cmd/*.go
+echo Done
+
+session="glink"
+
+tmux new-session -d -s $session
+
+# In my tmux settings, windows starts with '1', not '0'
+window=1
+user=alice
+tmux rename-window -t $session:$window "main"
+tmux send-keys -t $session:$window "./main -db-path=dbs/$user/$user.db" C-m
+
+user=bob
+tmux split-window -h "./main -db-path=dbs/$user/$user.db"
+
+
+tmux attach-session -t $session
